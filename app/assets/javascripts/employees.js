955feb0a3f3f39edde077445e15ffe90934c6ecd
jamestong <jamestong@wish8.com>
2017-05-02 17:48:03 +0800

    render index page by vuejs

---
+++ app/assets/javascripts/employees.js
@@ -0,0 +1,23 @@
+Vue.component('employee-row', {
+  template: '#employee-row',
+  props: {
+    employee: Object
+  }
+})
+
+var employees = new Vue({
+  el: '#employees',
+  data: {
+    employees: []
+  },
+  ready: function() {
+    var that;
+    that = this;
+    $.ajax({
+      url: '/employees.json',
+      success: function(resp) {
+        that.employees = resp;
+      }
+    });
+  }
+});
