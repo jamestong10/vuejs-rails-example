4bc7928222de5236a06b548b938197443a7e64d8
jamestong <jamestong@wish8.com>
2017-05-02 16:45:48 +0800

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   root 'employees#index'
-  get 'employees/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :employees, expects: [:new, :edit]
 end
